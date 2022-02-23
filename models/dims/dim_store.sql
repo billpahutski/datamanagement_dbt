102dcea387b525ddc05124fba91b35ca69a14e3a
william.pahutski.2022@anderson.ucla.edu <william.pahutski.2022@anderson.ucla.edu>
2022-02-23 00:18:37 +0000

    init

--- models/dims/dim_store.sql
+++ models/dims/dim_store.sql
@@ -1,6 +1,16 @@
 {{ config(materialized='table') }}
 
 SELECT
-    actor_id as actor_key,
-    CONCAT_WS(' ', last_name, first_name ) as full_name
-FROM {{ ref('stg_store') }}
+    stg_store.store_id as staff_id,
+
+    stg_address.district as district,
+    stg_address.address as address,
+    stg_address.phone as phone,
+
+    stg_staff.last_name as last_name
+FROM
+    {{ ref('stg_store') }} 
+    LEFT JOIN {{ ref('stg_address') }}
+        ON stg_store.address_id = stg_address.address_id
+    LEFT JOIN {{ ref('stg_staff') }}
+        ON stg_store.manager_staff_id = stg_staff.staff_id
